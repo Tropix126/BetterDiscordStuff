a29096b855c1b3e80774c580341ced66667d3395
Tropical <42101043+Tropix126@users.noreply.github.com>
2019-12-15 11:02:59 -0600

    switch to rellax

--- midnight/midnight-website/js/scripts.js
+++ midnight/midnight-website/js/scripts.js
@@ -1,37 +1,9 @@
-$.fn.moveIt = function(){
-  var $window = $(window);
-  var instances = [];
-
-  $(this).each(function(){
-    instances.push(new moveItItem($(this)));
-  });
-
-  window.addEventListener('scroll', function(){
-    var scrollTop = $window.scrollTop();
-    instances.forEach(function(inst){
-      inst.update(scrollTop);
-    });
-  }, {passive: true});
-}
-
-var moveItItem = function(el){
-  this.el = $(el);
-  this.speed = parseInt(this.el.attr('data-scroll-speed'));
-};
-
-moveItItem.prototype.update = function(scrollTop){
-  this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
-};
-
-// Initialization
-$(function(){
-  $('[data-scroll-speed]').moveIt();
-});
+var rellax = new Rellax('.rellax');
 
 //fade out on scroll -gibbu
 $(document).ready(function() {
 $(window).scroll(function() {
-    $('.top.title, .description, .primary-button').css("opacity", 1 - $(window).scrollTop() / 175)
+    $('.top.title, .description, .button-container').css("opacity", 1 - $(window).scrollTop() / 175)
 });
 });
 
